6554d319d2712e9d1ec1b36a083caeb5464b91ff
nautrw <nautrw@proton.me>
2025-01-06 21:50:09 -0500

    feat: add function to get name from note

--- mtheorypy/notes.py
+++ mtheorypy/notes.py
@@ -6,3 +6,21 @@ class Note:
     number: int
     length: int
     octave: int
+
+
+def get_note_name(note: Note, accidental: str = "#"):
+    if note.number not in range(11):
+        raise IndexError(f"note number out of bounds (0-11): {note.number}")
+
+    if accidental not in ["#", "b"]:
+        raise ValueError("argument `accidental` must be either `#` or `b`")
+
+    sharp_notes = ["C", "C#", "D", "D#", "E",
+                   "F", "F#", "G", "G#", "A", "A#", "B"]
+    flat_notes = ["C", "Db", "D", "Eb", "E",
+                  "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+
+    if accidental == "#":
+        return sharp_notes[note.number]
+    else:
+        return flat_notes[note.number]
