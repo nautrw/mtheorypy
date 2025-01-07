733ba01e1ac2c41f792d256f5d88827494e8e150
nautrw <nautrw@proton.me>
2025-01-06 22:00:31 -0500

    refactor: move get_note_name inside of note dataclass

--- mtheorypy/notes.py
+++ mtheorypy/notes.py
@@ -7,20 +7,20 @@ class Note:
     length: int
     octave: int
 
+    def get_name(self, accidental: str = "#"):
+        if self.number not in range(11):
+            raise IndexError(
+                f"note number out of bounds (0-11): {self.number}")
 
-def get_note_name(note: Note, accidental: str = "#"):
-    if note.number not in range(11):
-        raise IndexError(f"note number out of bounds (0-11): {note.number}")
+        if accidental not in ["#", "b"]:
+            raise ValueError("argument `accidental` must be either `#` or `b`")
 
-    if accidental not in ["#", "b"]:
-        raise ValueError("argument `accidental` must be either `#` or `b`")
+        sharp_notes = ["C", "C#", "D", "D#", "E",
+                       "F", "F#", "G", "G#", "A", "A#", "B"]
+        flat_notes = ["C", "Db", "D", "Eb", "E",
+                      "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 
-    sharp_notes = ["C", "C#", "D", "D#", "E",
-                   "F", "F#", "G", "G#", "A", "A#", "B"]
-    flat_notes = ["C", "Db", "D", "Eb", "E",
-                  "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-
-    if accidental == "#":
-        return sharp_notes[note.number]
-    else:
-        return flat_notes[note.number]
+        if accidental == "#":
+            return sharp_notes[self.number]
+        else:
+            return flat_notes[self.number]
