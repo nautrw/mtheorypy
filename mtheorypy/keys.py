028fb15db8f3b42a59919d37d4aecdb156ba2eff
nautrw <nautrw@proton.me>
2025-01-09 23:01:03 -0500

    refactor: use numeric values for notes to better standardize it

--- mtheorypy/keys.py
+++ mtheorypy/keys.py
@@ -4,24 +4,24 @@ from notes import CIRCLE_OF_FIFTHS
 
 # (Major, Minor)
 KEYS = [
-    ("Cb", "Ab"),
-    ("Gb", "Eb"),
-    ("Db", "Bb"),
-    ("Ab", "F"),
-    ("Eb", "C"),
-    ("Bb", "G"),
-    ("F", "D"),
-    ("C", "A"),
-    ("G", "E"),
-    ("D", "B"),
-    ("A", "F#"),
-    ("E", "C#"),
-    ("B", "G#"),
-    ("F#", "D#"),
-    ("C#", "A#"),
+    (11, 8),  # Cb to Ab
+    (6, 3),   # Gb to Eb
+    (1, 10),  # Db to Bb
+    (8, 5),   # Ab to F
+    (3, 0),   # Eb to C
+    (10, 7),  # Bb to G
+    (5, 2),   # F to D
+    (0, 9),   # C to A
+    (7, 4),   # G to E
+    (2, 11),  # D to B
+    (9, 6),   # A to F#
+    (4, 1),   # E to C#
+    (11, 8),  # B to G#
+    (6, 3),   # F# to D#
+    (1, 10),  # C# to A#
 ]
 
-BASE_SCALE = ["C", "D", "E", "F", "G", "A", "B"]
+BASE_SCALE = [0, 2, 4, 5, 7, 9, 11]
 
 
 @dataclass
@@ -51,3 +51,7 @@ class Key:
                 signature.append(f"{CIRCLE_OF_FIFTHS[i]}#")
 
         return signature
+
+
+key = Key(-2)
+print(key.get_names())
