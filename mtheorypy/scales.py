db1cd083b0600669dcdbf9c052d78e1138aed722
nautrw <nautrw@proton.me>
2025-01-12 16:35:08 -0500

    refactor: change system of getting the output of get scales

--- mtheorypy/scales.py
+++ mtheorypy/scales.py
@@ -1,36 +1,32 @@
-from dataclasses import dataclass
-
 from keys import Key
 
 BASE_SCALE = [0, 2, 4, 5, 7, 9, 11]
 
 
-@dataclass
+def get_base(root):
+    base_scale = []
+    current_root = root
+    for i in range(7):
+        base_scale.append((current_root + BASE_SCALE[i]) % 12)
+        current_root = (current_root + 12) % 12
+
+    return base_scale
+
+
+def modify(base_scale, indices, flatten: bool = True):
+    return [(v + (-1 if flatten else 1)) % 12 if i in indices
+            else v for i, v in enumerate(base_scale)]
+
+
 class Scale(Key):
-    def get_scales(self):
-        self.validate()
-        root = self.sharps % 12
-
-        base_scale = []
-        current_root = root
-        for i in range(7):
-            base_scale.append((current_root + BASE_SCALE[i]) % 12)
-            current_root = (current_root + 12) % 12
-
-        def modify(indices, flatten: bool = True):
-            return [(v + (-1 if flatten else 1)) % 12 if i in indices
-                    else v for i, v in enumerate(base_scale)]
-
-        # TODO: ADD MORE MODES
-        scales_dict = {
-            "major": base_scale,
-            "minor": modify([2, 5, 6]),
-            "pentatonic": base_scale[:3] + base_scale[4:6],
-            "dorian": modify([2, 6]),
-            "phrygian": modify([1, 2, 5, 6]),
-            "lydian": modify([3], False),
-            "mixolydian": modify([6]),
-            "locrian": modify([1, 2, 4, 5, 6])
-        }
-
-        return scales_dict
+    def __init__(self, sharps):
+        super().__init__(sharps)
+        self.root = self.sharps % 12
+        self.major = get_base(self.root)
+        self.minor = modify(self.major, [2, 5, 6])
+        self.pentatonic = self.major[:3] + self.major[4:6]
+        self.dorian = modify(self.major, [2, 6])
+        self.phrygian = modify(self.major, [1, 2, 5, 6])
+        self.lydian = modify(self.major, [3], False)
+        self.myxolidian = modify(self.major, [6])
+        self.locrian = modify(self.major, [1, 2, 4, 5, 6])
